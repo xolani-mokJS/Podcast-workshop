7fe58e98902905e7f7cf7f04b5a18b72b2056867
Xolani Mokhoebane <70220609+xolani-mokJS@users.noreply.github.com>
2022-11-12 10:15:06 +0200

    init commit
    initial commit

---
+++ api/api.js
@@ -0,0 +1,95 @@
+/***
+ * @typedef {object} preview
+ * @property {string} id
+ * @property {string} title
+ * @property {string} image
+ * @property {number} seasons
+ * @property {string[]} genre
+ * @property {string} updated
+ */
+
+/**
+ * @typedef {object} episode
+ * @property {number} episode
+ * @property {string} description
+ * @property {string} title
+ * @property {string} file
+ */
+
+/**
+ * @typedef {object} season
+ * @property {number} season
+ * @property {string} title
+ * @property {string} image
+ * @property {episode[]} episodes
+ */
+
+const newList = document.getElementById('newList');
+
+const getPodcasts = async ()=> {
+    const res = await fetch ("https://podcast-api.netlify.app/shows");
+
+    if (!res.ok) {
+        newList.innerHTML = "Could not get any Podcoasts";
+        return
+    }
+    
+    /**
+     * @type {preview[]}
+     */
+
+    const data = await res.json();
+
+    let newHtml =  '';
+
+    for (const { image, id, title, seasons} of data){
+            newHtml = `
+            ${newHtml}
+            <li class="list">
+            <image class="preview-image" src="${image}">
+            <button data-preview-button ="${id}">${title} <span>(${seasons})</span></button>
+            </li>`
+    }
+
+    newList.innerHTML = newHtml;
+
+    }
+
+    /**
+     * @param {string} id
+     */
+
+    const getSinglePodcast = async (id) => {
+        newList.innerHTML = `Loading...`;
+
+        const res = await fetch (`https://podcast-api.netlify.app/id/$(id)`);
+
+        if (!res.ok) {
+            newList.innerHTML = "Could not get podcast";
+            return
+        }
+
+        /** 
+         * @type {show}
+         */
+
+        const data = await res.json();
+
+        let seasosnsList =  '';
+
+        for (const {title, image} of data.seasons){
+            seasosnsList = `
+            ${seasosnsList}
+
+            <li class="list">
+            <image class="preview-image" src="${image}">
+            ${title}
+            </li>
+            `
+
+    }
+
+}
+
+getPodcasts();
+
